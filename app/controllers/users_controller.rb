399e2baa6a3ea6dd8c67f1242e32d881d4725fc9
giri681 <giri@tabashitechnology.com>
2018-03-09 10:32:22 +0530

    welcome page changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
   
   def index
-   @school = User.where("role = ?","school")
+   @school 	= User.where(:role => "school")
+   @teacher = User.where(:role => "teacher")
    @student = User.where(:role => "student")
+   @vendor 	= User.where(:role => "vendor")
   end
 
   def search
-  	@school = User.where("role = ?","school")
+  	@school = User.where(:role => "school")
   end
 
 end
