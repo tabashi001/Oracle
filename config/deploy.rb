826ffef46a20ae138d79f59f6055a973f40d91de
tanmay <tanmay@tabashitechnology.com>
2018-03-26 12:13:31 +0530

    master captcha commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,70 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.1"
+set :application, "EduOracle"
+set :repo_url, "git@github.com:tabashi001/Oracle.git"
+set :branch, 'master'
+set :user, 'ubuntu'
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :deploy_to, "/home/ubuntu/www/project"
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+# set :linked_files, %w(config/application.yml)
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp', 'public/system', "public/downloads", "public/assets")
+#sidekiq
+# set :sidekiq_role, [:app]
+# set :sidekiq_pid => File.join(current_path, 'tmp', 'pids', 'sidekiq.pid')
+# set :sidekiq_log => File.join(shared_path, 'log', 'sidekiq.log')
+# set :sidekiq_options, "-q default -q mailers,7 -q critical,5"
+# set :sidekiq_env =>  'production'
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+  before :start, :make_dirs
+end
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/master`
+        puts "WARNING: HEAD is not the same as origin/master"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
 
-# Default value for :pty is false
-# set :pty, true
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  #after  :finishing,    :restart
+end
