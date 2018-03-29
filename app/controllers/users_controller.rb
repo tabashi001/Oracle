5a9325828d106ffd044f203f715120df45564f01
tanmay <tanmay@tabashitechnology.com>
2018-03-29 12:45:44 +0530

    school shoecase bg image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,7 +25,7 @@ class UsersController < ApplicationController
       @school1 = User.where(:id => params[:myparams]).paginate(:per_page => 1, :page => params[:page])
       render json: {:event => @school1}
     elsif @@first_value.present?
-      binding.pry
+      #binding.pry
      @school = User.where("city LIKE ? AND role = ?","%#{@@first_value[:city]}%","#{@@first_value[:search_role]}").paginate(:per_page => 4, :page => params[:page])
     
     else
