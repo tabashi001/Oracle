1197aff7637962792fb78704a6b3f0831163ae1c
manishuti <manish.anand@utradefair.com>
2018-04-30 23:51:58 +0530

    added reviews to school overview page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
-
+  before_action :check_user_signed_in
   # GET /reviews
   # GET /reviews.json
   def index
@@ -14,9 +14,8 @@ class ReviewsController < ApplicationController
 
   # GET /reviews/new
   def new
-    binding.pry
     @college = User.find params[:user_id]
-    @review = Review.new
+    @review = @college.reviews.build
   end
 
   # GET /reviews/1/edit
@@ -26,17 +25,10 @@ class ReviewsController < ApplicationController
   # POST /reviews
   # POST /reviews.json
   def create
-    @review = Review.new(review_params)
-
-    respond_to do |format|
-      if @review.save
-        format.html { redirect_to @review, notice: 'Review was successfully created.' }
-        format.json { render :show, status: :created, location: @review }
-      else
-        format.html { render :new }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
-    end
+    @college = User.find params[:user_id]
+    @review = @college.reviews.build review_params
+    @review.save
+    redirect_to user_path(@college)
   end
 
   # PATCH/PUT /reviews/1
@@ -71,6 +63,6 @@ class ReviewsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
-      params.require(:review).permit(:rating, :comment)
+      params.require(:review).permit(:rating, :comment, :reviewer_id)
     end
 end
